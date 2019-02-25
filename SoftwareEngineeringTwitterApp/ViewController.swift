85809f9288f02c52830ea72d33efad4a4262727d
Burak Akin <burakakin@buraks.local>
2019-02-26 00:05:37 +0300

    Initial Commit

---
+++ SoftwareEngineeringTwitterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftwareEngineeringTwitterApp
+//
+//  Created by Burak Akin on 26.02.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
