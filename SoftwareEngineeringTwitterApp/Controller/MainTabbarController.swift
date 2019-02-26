ffe0a892bf5bddf76ee4eb0e6245e19cf01779ef
Burak Akin <burakakin@buraks.local>
2019-02-26 03:09:03 +0300

    TabbarController was implemented

---
+++ SoftwareEngineeringTwitterApp/Controller/MainTabbarController.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabbarController.swift
+//  SoftwareEngineeringTwitterApp
+//
+//  Created by Burak Akin on 26.02.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let homeFeedVC = storyboard.instantiateViewController(withIdentifier: "HomeFeedVC") as? HomeFeedVC else {
+            print("Couldn' instantiate HomeFeedVC")
+            return
+        }
+        guard let SearchVC = storyboard.instantiateViewController(withIdentifier: "SearchVC") as? SearchVC else {
+            print("Couldn' instantiate SearchVC")
+            return
+        }
+        guard let ProfileVC = storyboard.instantiateViewController(withIdentifier: "ProfileVC") as? ProfileVC else {
+            print("Couldn' instantiate ProfileVC")
+            return
+        }
+        
+        let homeFeed = createController(title: "Home", VC: homeFeedVC)
+        let search = createController(title: "Search", VC: SearchVC)
+        let profile = createController(title: "Profile", VC: ProfileVC)
+        
+        viewControllers = [homeFeed, search, profile]
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
+
+
+extension MainTabbarController {
+    
+    private func createController(title: String, VC: UIViewController) -> UINavigationController {
+        
+        let recentVC = UINavigationController(rootViewController: VC)
+        recentVC.tabBarItem.title = title
+        //recentVC.tabBarItem.image = UIImage(named: imageName)
+        
+        return recentVC
+        
+    }
+}
---
+++ SoftwareEngineeringTwitterApp/Controller/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  SoftwareEngineeringTwitterApp
+//
+//  Created by Burak Akin on 26.02.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
